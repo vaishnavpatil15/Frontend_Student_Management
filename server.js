2d2a7f522cc056855864f9b5d79cd9765a45e44c
Rohit Patil <patilrohit298@gmail.com>
2025-01-04 23:22:34 +0530

    add public files

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 
 // Set the views directory
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static("public"));
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
