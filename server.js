34096eaa59692fe1256e0b3881ca6573d0354015
Rohit Patil <patilrohit298@gmail.com>
2025-01-05 16:15:50 +0530

    - Add body parser support - Create form for create user - Add 404 user support - Update login partial for user name

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+let bodyParser = require('body-parser')
 
 const app = express();
 const PORT = 8000;
@@ -12,22 +13,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Route to render the home page
 app.get('/', async (req, res) => {
     try {
-        // Make a request to an external API
-        // const response = await axios.get('http://localhost:3000/api/departments/');
-        let response = {
-            data: {
-                data: []
-            }
-        }
-
-        // Pass the data to the EJS template
-        res.render('index', { data: response.data.data });
+        res.render('index', { data: {} });
     } catch (error) {
         console.error('Error fetching data:', error.message);
         res.status(500).send('An error occurred while fetching data.');
@@ -37,6 +30,29 @@ app.get('/', async (req, res) => {
 app.get('/login', async (req, res) => {
     res.render('login', {});
 
+});
+
+
+
+app.post('/createuser', async (req, res) => {
+    let requestData =  req.body
+    const response = await axios.post('http://localhost:3000/api/users/create', {  
+        "f_name": requestData.FirstName,
+        "l_name": requestData.LastName,
+        "user_id": requestData.UserId,
+        "dept_id": requestData.dept,
+        "password": requestData.Password });
+    let pageData = []
+    if (response && response.data && response.data) {
+        console.log(response.data.message);
+        let createdUserData
+         const userData = await axios.get(`http://localhost:3000/api/users/${requestData.UserId}`);
+         if (userData && userData.data && userData.data.data.length > 0) {
+            createdUserData = userData.data.data[0]
+        }
+        res.render('index', { data: createdUserData });
+    }
+   
 });
 
 app.get('/register', async (req, res) => {
