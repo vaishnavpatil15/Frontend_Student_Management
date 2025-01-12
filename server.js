cd7a8dc26051deb5a7371233564c3a552b48baed
Rohit Patil <patilrohit298@gmail.com>
2025-01-12 23:09:04 +0530

    Remove unwanted package

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 let bodyParser = require('body-parser')
-const bcrypt = require('bcrypt')
 const session = require('express-session')
 const passport = require('passport');
 const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
